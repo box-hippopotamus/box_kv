66e55afdff5a3e442451e8fbfef37d1b82174f9f
盒马盒马 <2353151024@qq.com>
2026-01-20 21:21:35 +0800

    Delete sdk directory

--- sdk/rust/target/debug/deps/boxkv_sdk-d76fe245d4ca188f.d
+++
@@ -1,5 +0,0 @@
-D:\project\box_kv\sdk\rust\target\debug\deps\boxkv_sdk-d76fe245d4ca188f.d: src\lib.rs
-
-D:\project\box_kv\sdk\rust\target\debug\deps\libboxkv_sdk-d76fe245d4ca188f.rmeta: src\lib.rs
-
-src\lib.rs:
